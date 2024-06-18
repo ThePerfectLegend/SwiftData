8a859b3efeb8bfba11beeef791ea1e5ee5485aec
Nizami Tagiyev <94032706+ThePerfectLegend@users.noreply.github.com>
2024-06-18 15:01:27 +0700

    Initial Commit

---
+++ Swift Data/Swift_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DataApp.swift
+//  Swift Data
+//
+//  Created by Nizami Tagiyev on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
